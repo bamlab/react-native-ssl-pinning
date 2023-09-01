a88cd747b548d08dffc1140a513780b8f728c0f1
Hugo K <hugo.khlaut@student-cs.fr>
2023-09-01 17:43:51 +0200

    plugin added

--- android/src/main/java/expo/modules/configpluginsslpinning/SSLPinningView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.configpluginsslpinning
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class SSLPinningView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
---
+++ src/withSSLPinningConfig.ts
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+import { ConfigPlugin } from "@expo/config-plugins";
+
+const withSSLPinningConfig: ConfigPlugin<{
+  sslConfig: {
+    hostName: string;
+    certificateSHAFinal: string;
+    certificateSHAIntermediate: string;
+    certificateSHARoot: string;
+  };
+}> = (config, { sslConfig }) => {
+  const path = "modules/security/android/sslpinning.properties";
+  if (fs.existsSync(path)) {
+    fs.unlinkSync(path);
+  }
+
+  // Create the ssl.properties file from the sslConfig object
+  let data = "";
+  for (const [key, value] of Object.entries(sslConfig)) {
+    data += `${key}=${value}\n`;
+  }
+
+  // Write file in a sync way makes pod crashes
+  fs.writeFile(path, data, { encoding: "utf8" }, () => {});
+  return config;
+};
