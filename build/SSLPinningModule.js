53186d90df7748afce03d2b516efd69089949b79
Hugo K <hugo.khlaut@student-cs.fr>
2023-10-12 17:05:50 +0200

    chore(build): gitignore build folder

--- build/SSLPinningModule.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const expo_modules_core_1 = require("expo-modules-core");
-// It loads the native module object from the JSI or falls back to
-// the bridge module (from NativeModulesProxy) if the remote debugger is on.
-exports.default = (0, expo_modules_core_1.requireNativeModule)('SSLPinning');
-//# sourceMappingURL=SSLPinningModule.js.map
--- build/withSSLPinningConfig.d.ts
+++
@@ -1,3 +0,0 @@
-declare const withSSLPinningConfig: (config: any, sslConfig: any) => any;
-export default withSSLPinningConfig;
-//# sourceMappingURL=withSSLPinningConfig.d.ts.map
